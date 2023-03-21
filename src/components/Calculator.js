bdcb1da7b0fc8bc3c1de698e0a8f0753002dede8
henry-dura <henryikemefuna@gmail.com>
2023-03-21 22:46:25 +0100

    Delete BoilerPlate and create calculator

---
+++ src/components/Calculator.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Screen = () => (
+  <div className="screen">
+    <p>0</p>
+  </div>
+);
+
+const CalculatorUI = () => (
+  <div className="calculator">
+    <Screen />
+
+    <div className="keys">
+      <button type="button">AC</button>
+      <button type="button">+/-</button>
+      <button type="button">%</button>
+      <button type="button" className="operation">÷</button>
+    </div>
+    <div className="keys">
+      <button type="button">7</button>
+      <button type="button">8</button>
+      <button type="button">9</button>
+      <button type="button" className="operation">x</button>
+    </div>
+    <div className="keys">
+      <button type="button">6</button>
+      <button type="button">5</button>
+      <button type="button">4</button>
+      <button type="button" className="operation">-</button>
+    </div>
+    <div className="keys">
+      <button type="button">3</button>
+      <button type="button">2</button>
+      <button type="button">1</button>
+      <button type="button" className="operation">+</button>
+    </div>
+    <div className="keys">
+      <button type="submit" className="zero">0</button>
+      <button type="button" className="dot">.</button>
+      <button type="button" className="operation">=</button>
+    </div>
+  </div>
+);
+
+export default CalculatorUI;
