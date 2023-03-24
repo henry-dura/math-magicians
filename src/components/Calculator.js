94c8fe1b220ecc5fcb0bd7c5ba6aee1bf0bf2b72
henry-dura <henryikemefuna@gmail.com>
2023-03-24 01:06:17 +0100

    Add funtionality to buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,41 +1,52 @@
-import React from 'react';
-import Screen from './Screen';
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 
-const CalculatorUI = () => (
-  <div className="calculator">
-    <Screen />
+const CalculatorUI = () => {
+  const [state, setState] = useState({ total: 0, next: null, operation: null });
 
-    <div className="keys">
-      <button type="button">AC</button>
-      <button type="button">+/-</button>
-      <button type="button">%</button>
-      <button type="button" className="operation">÷</button>
-    </div>
+  function HandleBtnClick(event) {
+    setState((state) => calculate(state, event.target.name));
+  }
 
-    <div className="keys">
-      <button type="button">7</button>
-      <button type="button">8</button>
-      <button type="button">9</button>
-      <button type="button" className="operation">x</button>
+  return (
+    <div className="calculator">
+      <div className="screen">
+        {state.total}
+        {state.operation}
+        {state.next}
+      </div>
+      <div className="keys">
+        <button type="button" name="AC" onClick={HandleBtnClick}>AC</button>
+        <button type="button" name="+/-" onClick={HandleBtnClick}>+/-</button>
+        <button type="button" name="%" onClick={HandleBtnClick}>%</button>
+        <button type="button" className="operation" name="÷" onClick={HandleBtnClick}>÷</button>
+      </div>
+
+      <div className="keys">
+        <button type="button" name="7" onClick={HandleBtnClick}>7</button>
+        <button type="button" name="8" onClick={HandleBtnClick}>8</button>
+        <button type="button" name="9" onClick={HandleBtnClick}>9</button>
+        <button type="button" className="operation" name="x" onClick={HandleBtnClick}>x</button>
+      </div>
+      <div className="keys">
+        <button type="button" name="6" onClick={HandleBtnClick}>6</button>
+        <button type="button" name="5" onClick={HandleBtnClick}>5</button>
+        <button type="button" name="4" onClick={HandleBtnClick}>4</button>
+        <button type="button" className="operation" name="-" onClick={HandleBtnClick}>-</button>
+      </div>
+      <div className="keys">
+        <button type="button" name="3" onClick={HandleBtnClick}>3</button>
+        <button type="button" name="2" onClick={HandleBtnClick}>2</button>
+        <button type="button" name="1" onClick={HandleBtnClick}>1</button>
+        <button type="button" className="operation" name="+" onClick={HandleBtnClick}>+</button>
+      </div>
+      <div className="keys">
+        <button type="button" className="zero" name="0" onClick={HandleBtnClick}>0</button>
+        <button type="button" className="dot" name="." onClick={HandleBtnClick}>.</button>
+        <button type="button" className="operation" name="=" onClick={HandleBtnClick}>=</button>
+      </div>
     </div>
-    <div className="keys">
-      <button type="button">6</button>
-      <button type="button">5</button>
-      <button type="button">4</button>
-      <button type="button" className="operation">-</button>
-    </div>
-    <div className="keys">
-      <button type="button">3</button>
-      <button type="button">2</button>
-      <button type="button">1</button>
-      <button type="button" className="operation">+</button>
-    </div>
-    <div className="keys">
-      <button type="submit" className="zero">0</button>
-      <button type="button" className="dot">.</button>
-      <button type="button" className="operation">=</button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default CalculatorUI;
