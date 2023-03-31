03d86ff6d66e0e43b80fb1c33bff57c678d47d02
henry-dura <henryikemefuna@gmail.com>
2023-03-31 19:23:35 +0100

    Add route to Nav links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CalculatorUI from './components/Calculator';
+import NavBar from './components/NavBar';
 import FetchData from './components/Quote';
+import Home from './components/Home';
 
 function App() {
   return (
-    <>
-      <CalculatorUI />
-      <FetchData />
-    </>
+    <Router>
+      <NavBar />
+      <div className="content">
+        <Switch>
+          <Route path="/" exact><Home /></Route>
+          <Route path="/calculator"><CalculatorUI /></Route>
+          <Route path="/quotes"><FetchData /></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+const Home = () => (
+  <>
+    <h1>THIS IS HOMEZ</h1>
+  </>
+);
+
+export default Home;
